56e756daa1e18febf32e2579b294934802f2ca91
“Ranjit <ranjit.saimbi@gmail.com>
2019-04-15 19:01:41 +0100

    some flags

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,11 @@
 class GamesController < ApplicationController
 
   def create
-    
+    @user = User.find(params[:id])
+    @game = @user.games.build(score: 0)
+    byebug
+    # create a game object and create associated questions
+    # redirect to question number 1 using query
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Question.destroy_all
 
 ranjit = User.create(name: "Ranjit")
+
+Question.create(question_type: "Flag", location: 'flags/zw.png', answer: "Zimbabwe")
+Question.create(question_type: "Flag", location: 'flags/zm.png', answer: "Zambia")
+Question.create(question_type: "Flag", location: 'flags/za.png', answer: "South Africa")
+Question.create(question_type: "Flag", location: 'flags/ye.png', answer: "Yemen")
+Question.create(question_type: "Flag", location: 'flags/ws.png', answer: "Samoa")
+Question.create(question_type: "Flag", location: 'flags/vu.png', answer: "Vanuatu")
+
+# <%= image_tag(image_url('flags/ad.png'), size: "640x400")%>
