95f8d5abc68d3bcef9f937083b49fc1541858b2e
“Ranjit <ranjit.saimbi@gmail.com>
2019-04-16 09:22:06 +0100

    LATEST

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,19 +1,19 @@
 class ResultsController < ApplicationController
 
-  def create
-  5.times do
-  @result = Result.create(
-  game_id: params[:result][:game_id],
-  question_id: #{Question.all.sample.id}
-  )
-  redirect_to @result
-  end
-  end
-
-
-  def show
-    @result = Result.find(params[:id])
-  end
+  # def create
+  # 5.times do
+  # @result = Result.create(
+  # game_id: params[:result][:game_id],
+  # question_id: #{Question.all.sample.id}
+  # )
+  # redirect_to @result
+  # end
+  # end
+  #
+  #
+  # def show
+  #   @result = Result.find(params[:id])
+  # end
 
 
 
