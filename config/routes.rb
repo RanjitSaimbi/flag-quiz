5df7560ef3437819aa568df46cc7c519f8b669be
keoki33 <gwong323@yahoo.com>
2019-04-16 08:28:44 +0100

    more pages and cats images

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: :show
+  resources :users
   resources :results
   resources :games
   resources :questions
+  get '/', to: 'users#welcome', as: 'welcome'
 
 end
