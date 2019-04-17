d1ba2c7ecca29b595074f5f4e104ed45911c474e
keoki33 <gwong323@yahoo.com>
2019-04-17 14:15:53 +0100

    boots

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,7 +3,7 @@ class GamesController < ApplicationController
   def create
     @user = User.find(params[:id])
     @game = @user.games.create(score: 0)
-    random_question_ids = Question.all.map {|q|q.id}.sample 5
+    random_question_ids = Question.all.map {|q|q.id}.sample 10
     random_question_ids.each {|id|
       @game.results.create(question_id: id)
     }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_many :games
+
+
+  validates :name, uniqueness:  { message: "error: Your twin is already registered, make up a new name" }
+  validates :name, presence: { message: "error: Everybody has a name, please enter it" }
+
 end
